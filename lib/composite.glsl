37112a181bbb8afda6dd89ab2fb0aa048d1fd973
archmoj <mojtaba@plot.ly>
2019-11-10 22:45:52 -0500

    use power of three

--- lib/composite.glsl
+++ lib/composite.glsl
@@ -3,6 +3,12 @@ precision mediump float;
 uniform sampler2D accumBuffer;
 varying vec2 uv;
 
+const float n = 3.0;
+
+float f(float x) {
+  return pow(x, n);
+}
+
 void main() {
   vec4 accum = texture2D(accumBuffer, 0.5 * (uv + 1.0));
   float r = accum.r;
@@ -10,7 +16,7 @@ void main() {
   float b = accum.b;
   float a = accum.a;
 
-  float q = 1.0 - (r + g + b) / 3.0;
+  float q = pow(1.0 - (f(r) + f(g) + f(b)) / 3.0, 1.0 / n);
 
   gl_FragColor = vec4(
     r * q,
